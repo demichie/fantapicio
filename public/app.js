2444ff1c5aa2d00c635d27b221002eee7daf1cee
Mattia de' Michieli Vitturi <demichie@gmail.com>
2024-09-08 17:32:02 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -78,7 +78,7 @@ socket.on('playerNominated', (data) => {
 blockTimerButton.addEventListener('click', () => {
     socket.emit('getParticipants');
     const bidder = participants.find(p => p.name === userName);
-    if ( bidder.budget >= bidder.remainingPlayers ) {
+    if ( bidder.budget >= bidder.remainingPlayers && userName != currentBidder ) {
         document.getElementById('bid-section').style.display = 'block';
         socket.emit('blockTimer', userName);
         document.getElementById('bidder-section').style.display = 'none';
