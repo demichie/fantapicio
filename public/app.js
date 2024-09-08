de186b33c37fc4e4e5487428a0aec89eb31910f0
Mattia de' Michieli Vitturi <demichie@gmail.com>
2024-09-08 09:32:35 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -63,6 +63,7 @@ socket.on('playerNominated', (data) => {
 // Block the timer when the button is clicked
 blockTimerButton.addEventListener('click', () => {
     document.getElementById('bid-section').style.display = 'block';
+    document.getElementById('block-section').style.display = 'none';
     socket.emit('blockTimer', userName);
 });
 
@@ -70,7 +71,6 @@ blockTimerButton.addEventListener('click', () => {
 socket.on('blockTimer', (bidderName) => {
     blockTimerButton.textContent = `${bidderName} is bidding`;
     blockTimerButton.classList.add('red');
-    document.getElementById('block-section').style.display = 'none';
 });
 
 // Place a bid
